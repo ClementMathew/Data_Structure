8ea2875eb2ecb0b46d8aabc5d45718f7b323ab89
ClementMathew <clementmathew924@gmail.com>
2024-07-19 14:02:05 +0530

    modifications

--- Binary_Search_Tree_Sort.c
+++ Binary_Search_Tree_Sort.c
@@ -1,65 +1,66 @@
 
-// Binary Search Tree 
+// Binary Search Tree
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 50
 
 struct Node
 {
     int data;
-    struct Node* left;
-    struct Node* right; 
+    struct Node *left;
+    struct Node *right;
 };
 
-struct Node * NewNode(int x)
+struct Node *NewNode(int x)
 {
-    struct Node * temp = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
     temp->data = x;
     temp->left = NULL;
     temp->right = NULL;
     return temp;
 }
 
-struct Node * insert(struct Node * root,int x)
+struct Node *insert(struct Node *root, int x)
 {
-    if(root == NULL)
+    if (root == NULL)
     {
         root = NewNode(x);
     }
-    else if(root->data >= x)
+    else if (root->data >= x)
     {
-        root->left = insert(root->left,x);
+        root->left = insert(root->left, x);
     }
     else
     {
-        root->right = insert(root->right,x);
+        root->right = insert(root->right, x);
     }
     return root;
 }
 
-void inOrder(struct Node* root)
+void inOrder(struct Node *root)
 {
-    if(root!=NULL)
+    if (root != NULL)
     {
         inOrder(root->left);
-        printf("%d\t",root->data);
+        printf("%d\t", root->data);
         inOrder(root->right);
     }
 }
 
 int main()
 {
-    struct Node* root=NULL;
-    int x,n;
+    struct Node *root = NULL;
+    int x, n;
     printf("Enter no. of elements : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
+
     printf("Enter the elements : ");
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&x);
-        root = insert(root,x);
+        scanf("%d", &x);
+        root = insert(root, x);
     }
     printf("Sorted order is : ");
     inOrder(root);
